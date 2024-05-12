9a34b8fe50b93c90630b5fbb355b6e08c17ede7b
melvstein <melvinbayogo@gmail.com>
2024-05-12 11:24:24 +0800

    melvstein portfolio

--- src/app/components/DisplayPicture.tsx
+++ src/app/components/DisplayPicture.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import ProfilePic from "../../../public/images/melvstein-dp.jpg";
-import MelvsteinLogo from "../../../public/images/logo/melvstein_main_logo.png";
+import MjLogoWhite from "../../../public/images/logo/mj_logo_white.png";
+import MjLogoBlack from "../../../public/images/logo/mj_logo_black.png";
+import MjLogoVariant1 from "../../../public/images/logo/mj_logo_variant1.png";
+import MjLogoVariant2 from "../../../public/images/logo/mj_logo_variant2.png";
+import MjLogoVariant3 from "../../../public/images/logo/mj_logo_variant3.png";
 
 export default function DisplayPicture() {
     return (
         <div>
-            <Image src={MelvsteinLogo} width={50} height={50} alt="melvstein display picture" className="uppercase rounded-full"/>
+            <Image src={MjLogoVariant3} width={50} height={50} alt="melvstein display picture" className="uppercase"/>
         </div>
     );
 }
